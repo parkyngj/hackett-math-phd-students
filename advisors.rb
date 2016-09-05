5b7cdd8311ddc83a66727a7d970930c9d1ce09ad
Young Ju Park <parkyngj@gmail.com>
2016-09-04 22:11:08 -0700

    Add advisor search by field function

--- advisors.rb
+++ advisors.rb
@@ -17,3 +17,25 @@ def view_advisors
   puts "--------------------------------" if idx<select_query.length-1
   end
 end
+
+def search_advisors_by_field(field_id)
+  puts "================================\nSearch Advisors By Field ID: #{field_id}\n================================"
+  selected_advisors = $database.execute("select advisors.id, advisors.last_name, advisors.first_name from advisors where advisors.field=?", [field_id])
+
+  refined_selected_advisors = []
+
+  selected_advisors.each_with_index do |advisor|
+    refined_advisor = advisor.drop_while {|k,v| k!= 0}
+    refined_selected_advisors << refined_advisor
+  end
+
+  columns = ["Advisor ID", "Last Name", "First Name"]
+
+  refined_selected_advisors.each_with_index do |advisor, idx|
+    columns.each_with_index do |column, i|
+      puts "#{column}: #{advisor[i][1]}"
+    end
+
+  puts "--------------------------------" if idx<refined_selected_advisors.length-1
+  end
+end
